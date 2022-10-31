b85dffe26df96e7e43fc4d13be046bbc1fba554c
Eka Widiatno <103700152+eawidiatno@users.noreply.github.com>
2022-10-31 18:24:32 +0700

    .

---
+++ bin/ssh_application_1.dart
@@ -0,0 +1,5 @@
+import 'package:ssh_application_1/ssh_application_1.dart' as ssh_application_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ssh_application_1.calculate()}!');
+}
---
+++ test/ssh_application_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:ssh_application_1/ssh_application_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
